19ccf043e74f4c35526fecea0925505129424ca4
WO-VIER <virgile.wautier@gmail.com>
2025-03-29 17:57:34 +0100

    add gnl/libft

---
+++ gnl/get_next_line.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vwautier <vwautier@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 16:36:39 by vwautier          #+#    #+#             */
+/*   Updated: 2025/01/03 14:15:46 by vwautier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst);
+char	*ft_strdup(const char *s);
+size_t	size_content(t_list *list);
+char	*get_next_line(int fd);
+void	read_save(t_list **save, char *buffer, int fd);
+void	extract_line(t_list **list, char **ligne);
+void	clean_save(t_list **save);
+int		have_new_line_last(t_list *list);
+
+#endif
